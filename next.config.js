4ebf53abff2c2886dd0d7bb9a82e3c5022a300d6
richardsonmarkg <richardsonmarkg@gmail.com>
2025-04-28 17:35:31 +0000

    Remove obsolete experimental.appDir flag

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 // next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // keep this so you don’t get ESlint errors on Vercel
   eslint: {
     ignoreDuringBuilds: true,
   },
